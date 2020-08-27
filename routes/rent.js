ed52a47fcc8a6558caaa887d983fb08f20b6950b
Abhishek <kumar.bits009@gmail.com>
2020-08-27 13:08:16 +0530

    Returning movies feature added!

--- routes/rent.js
+++ routes/rent.js
@@ -1,8 +1,5 @@
 const mysqlConnection = require('../mysql/dbConnection');
 const express = require('express');
-const {
-    response
-} = require('express');
 const Router = express.Router();
 
 Router.post('/', (req, res) => {
@@ -47,5 +44,45 @@ Router.post('/', (req, res) => {
     });
 
 });
+
+Router.put('/', (req, res) => {
+    let body = req.body;
+    let phoneNumber = body.phone;
+    let movies = [];
+    delete body.phone;
+    for (key in body) {
+        movies.push(body[key]);
+    }
+    mysqlConnection.query("select customer_id from customers where phone_number = ?", [phoneNumber], (err, rows, fields) => {
+        if (!err) {
+            let customer_id = rows[0].customer_id;
+            query = "select * from rents where customer_id = ? and bar_code in (?)";
+            mysqlConnection.query(query, [customer_id, movies], (error, rows, fields) => {
+                if (!error) {
+                    //If returning the movie
+                    let rent_ids = [];
+                    for (let i = 0; i < rows.length; i++) {
+                        rent_ids.push(rows[i].rent_id);
+                    }
+                    queryReturn = "update rents set return_status_code = 2 where rent_id in (?)";
+                    mysqlConnection.query(queryReturn, [rent_ids], (err, response) => {
+                        if (!err) {
+                            res.send(`Successfully returned!`);
+                            console.log(response);
+                        } else {
+                            res.send(`Failed to update rent status!`);
+                        }
+                    })
+                } else {
+                    res.send(`Failed to load rents!`);
+                }
+
+            })
+        } else {
+            res.send(`Failed to load user!`)
+        }
+    })
+
+})
 
 module.exports = Router;
